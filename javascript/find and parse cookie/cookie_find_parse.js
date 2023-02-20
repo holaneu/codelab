af346e3c1f86c01a7e016bff72596fe39912e4ee
holaneu <20854147+holaneu@users.noreply.github.com>
2023-02-20 12:37:55 +0100

    Update cookie_find_parse.js

--- javascript/find and parse cookie/cookie_find_parse.js
+++ javascript/find and parse cookie/cookie_find_parse.js
@@ -1,17 +1,43 @@
-const cookieName = "OptanonConsent";
-const cookieValue = document.cookie.split("; ")
-  .find(cookie => cookie.startsWith(`${cookieName}=`))
-  .split("=")[1];
+function parseConsentCookie(){
+  var activeGroups = [];
+  var cookieName = "OptanonConsent";
+  var cookieValue;
+  var groupsValue;
+  var groupsValueDecoded;
 
-const groupsValue = cookieValue.split("&").find(pair => pair.startsWith("groups=")).split("=")[1];
+  cookieValue = document.cookie
+    .split("; ")
+    .find(function(item){
+      return item.startsWith(cookieName +'=')
+    });
+  
+  if (cookieValue) {
+    groupsValue = cookieValue
+    .split("&")
+    .find(function(pair){
+      return pair.startsWith("groups=")
+    })
+    .split("=")[1];
+  }
 
-const containsC00021 = groupsValue.includes("C0002:1");
-const containsC00020 = groupsValue.includes("C0002:0");
+  if (groupsValue) {
+    groupsValueDecoded = decodeURIComponent(groupsValue);
+  }  
 
-if (containsC00021) {
-  return true;
-} else if (containsC00020) {
-  return false;
-} else {
-  // handle case where neither substring is present
+  if (groupsValueDecoded) {
+    if(groupsValueDecoded.indexOf('C0001:1') > -1){
+      activeGroups.push('C0001');
+    }
+    if(groupsValueDecoded.indexOf('C0002:1') > -1){
+      activeGroups.push('C0002');
+    }
+    if(groupsValueDecoded.indexOf('C0003:1') > -1){
+      activeGroups.push('C0003');
+    }
+    if(groupsValueDecoded.indexOf('C0004:1') > -1){
+      activeGroups.push('C0004');
+    }
+  }
+
+  return activeGroups && activeGroups.length > 0 ? activeGroups.join(',') : undefined;  
 }
